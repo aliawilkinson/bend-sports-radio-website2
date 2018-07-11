024653562c8a42b27052e701398fe1aaeca6ac55
Alia Wilkinson <aliawilkinson@gmail.com>
2018-07-11 16:33:37 -0700

    added more information and footer styling

--- src/components/employee-profiles.js
+++ src/components/employee-profiles.js
@@ -1,13 +1,15 @@
 
 import React from 'react';
 import '../assets/css/employee-profiles.css';
-import tracee from '../assets/images/tracee.jpg';
+import tracee from '../assets/images/employees/tracee.jpg';
+import dan from '../assets/images/employees/dan-patrick.jpg';
+import colin from '../assets/images/employees/colin-cowherd.jpg';
 
 
 var employeeProfiles = [
-    { name: 'Tracee', img: tracee, show: 'Tracee Tuesdays' },
-    { name: 'Dan', img: tracee, show: 'The Dan Patrick Show' },
-    { name: 'Colin', img: tracee, show: 'The Herd' }
+    { name: 'Tracee Tuesday', img: tracee, show: 'The Fox Sports Report' },
+    { name: 'Dan Patrick', img: dan, show: 'The Dan Patrick Show' },
+    { name: 'Colin Cowherd', img: colin, show: 'The Herd with Colin Cowherd' }
 ]
 
 const Employees = () => {
@@ -15,21 +17,22 @@ const Employees = () => {
     const circularText = (txt, radius) => {
         var finalElements = [];
         txt = txt.split("");
-        var deg = 100 / txt.length,
-            origin = 0;
+        var deg = 60 / txt.length,
+            origin = 20;
 
         txt.forEach((ea) => {
             var index = Math.random();
             index++;
             var style = {
-                height: `${radius}px`,
+                height: `${radius}vw`,
                 position: `absolute`,
                 transform: `rotate(${origin}deg)`,
                 transformOrigin: `0 100%`,
-                backgroundColor: 'blue',
                 zIndex: '1',
-                top: '0',
-                right: '50%'
+                top: '-5vw',
+                right: '45%',
+                textShadow: '0 0 3px #FF0000, 0 0 5px #0000FF',
+                fontSize: '1em'
             };
 
             origin += deg;
@@ -43,17 +46,23 @@ const Employees = () => {
 
     const employees = employeeProfiles.map((employee, index) => {
         return (
-            <div key={index} className="person-cont">
-                <img src={employee['img']} />
-                {circularText(employee['name'], 100)}
-                {circularText(employee['show'], 100)}
+            <div key={index} className="employee-cont">
+                <h3>{employee['name']}</h3>
+                <div className="person-cont">
+
+                    <img src={employee['img']} />
+                    {/* {circularText(employee['name'], 30)} */}
+                    {/* {circularText(employee['show'], 32)} */}
+
+                </div>
+                <h3>{employee['show']}</h3>
             </div>
         )
     });
 
     return (
         <div>
-            <h2>Our Team</h2>
+            <h2>Our On-Air Team</h2>
             {employees}
         </div>
     )
