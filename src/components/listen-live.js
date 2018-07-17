7e07749ca1b28c16ade1c4ea4170f9b17066bae6
Alia Wilkinson <aliawilkinson@gmail.com>
2018-07-17 13:45:02 -0700

    custom live player working

--- src/components/listen-live.js
+++ src/components/listen-live.js
@@ -6,18 +6,37 @@ import '../assets/css/listen-live.css';
 class ListenLive extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            playing: false,
+            icon: "fa fa-play-circle play fa-3x"
+        }
     }
+
+    playAudio() {
+        this.setState({ playing: !this.state.playing });
+        debugger;
+        if (this.state.playing) {
+            this.player.pause();
+            this.setState({
+                icon: "fa fa-play-circle play fa-3x"
+            })
+        } else {
+            this.player.play();
+            this.setState({
+                icon: "fas fa-pause-circle fa-3x"
+            })
+        }
+    }
+
     render() {
         return (
-            <div id="audio_div">
-                <h2>
-                    <p>Listen Live</p>
-                    <div className="arrow-cont">
-                        <i class="fa fa-play-circle play fa-3x" aria-hidden="true"></i>
-                    </div>
-                </h2>
-                <div className="audio-ojb-wrap">
-                    <audio controls="play" className="player" preload="false">
+            <div className="listen-live-cont">
+                <p>Listen Live</p>
+                <div className="arrow-cont" onClick={this.playAudio.bind(this)}>
+                    <i className={this.state.icon} aria-hidden="true"></i>
+                </div>
+                <div>
+                    <audio ref={e => this.player = e} className="player" preload="false">
                         <source src="https://18543.live.streamtheworld.com/KCOEFMAAC_SC" />
                         Your browser does not support the audio element. Please switch to a modern browser (chrome, firefox, edge, etc.)
                     </audio>
