77aff4eba420173c30303d69bb57a367241100fe
Alia Wilkinson <aliawilkinson@gmail.com>
2018-07-23 19:23:41 -0700

    header scroll

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import photo1 from '../assets/images/in_studio1.jpg';
 import '../assets/css/about.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
         <div className="about">
             <img src={photo1} />
 
-            <p>
+            <div>
                 <h1>About</h1>
-                <br/>
+                <br />
                 FOX Sports 94.9 Central Oregon gives listeners the opportunity to step away from their everyday stresses of life and immerse themselves into the world of sports entertainment. FOX Sports 94.9 is THE destination for the ultimate sports fan, featuring legendary sports talk hosts and sports fan's audio nirvana in Central Oregon!
                 <br />
                 <br />
@@ -18,7 +18,7 @@ const About = () => {
                 <br />
                 <br />
                 <div><Link to="/contact">Contact Us</Link></div>
-                </p>
+            </div>
         </div>
     )
 }
