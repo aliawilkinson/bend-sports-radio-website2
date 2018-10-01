412330beb96672ed87032f665701202974a82ad2
Alia Wilkinson <aliawilkinson@gmail.com>
2018-09-30 17:19:26 -0700

    added page for 404 not found routes

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import '../assets/css/app.css';
 import LandingPage from './landing-page';
 import Footer from './footer';
 import Scoreboard from './scoreboard';
-import { Route } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+// import SplashPage from './login/SplashPage.jsx';
+// import NotFound from './NotFound.jsx';
 import About from './about';
 import Employees from './employee-profiles';
 import Advertise from './advertise';
@@ -12,6 +14,7 @@ import Employment from './employment';
 import ListenLive from './listen-live';
 import WinFreeStuff from './winfreestuff';
 import HeaderChange from './header';
+import NotFound from './not-found';
 import ReactGA from 'react-ga';
 
 const App = () => {
@@ -24,14 +27,17 @@ const App = () => {
             <Scoreboard />
             <HeaderChange />
             <ListenLive show="listen-on-app" />
-            <Route exact path="/" component={LandingPage} />
-            <Route path='/on-air-shows' component={Employees} />
-            <Route path='/win-free-stuff' component={WinFreeStuff} />
-            <Route path='/advertise' component={Advertise} />
-            <Route path='/about' component={About} />
-            <Route path='/contact' component={Contact} />
-            <Route path='/employment' component={Employment} />
-            <Route path='/listen-live' component={ListenLive} />
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route path='/on-air-shows' component={Employees} />
+                <Route path='/win-free-stuff' component={WinFreeStuff} />
+                <Route path='/advertise' component={Advertise} />
+                <Route path='/about' component={About} />
+                <Route path='/contact' component={Contact} />
+                <Route path='/employment' component={Employment} />
+                <Route path='/listen-live' component={ListenLive} />
+                <Route path='/*' component={NotFound} />
+            </Switch>
             <Footer />
         </div>
     )
