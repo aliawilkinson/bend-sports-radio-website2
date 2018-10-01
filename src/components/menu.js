7f3d358ebc452a89fd43bb48939460190ef5a861
Alia Wilkinson <aliawilkinson@gmail.com>
2018-09-30 18:03:37 -0700

    css changes to improve look on pcs

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,16 +12,40 @@ class Menu extends Component {
                 <div className={`menu-container ${this.props.size}`}>
                     <div className="links-cont">
                         <Link to="/on-air-shows">
-                            <h1><i className="fas fa-music"></i> On Air Shows</h1>
+                            <h1>
+                                <i className="fas fa-music"></i>
+                                <span> On Air Shows</span>
+                            </h1>
+                        </Link>
+                        <Link to="/win-free-stuff">
+                            <h1>
+                                <i className="fas fa-trophy"></i>
+                                <span> Win Free Stuff</span>
+                            </h1>
                         </Link>
                         <Link to="/advertise">
-                            <h1><i className="fas fa-bolt"></i> Advertise</h1>
+                            <h1>
+                                <i className="fas fa-bolt"></i>
+                                <span> Advertise</span>
+                            </h1>
                         </Link>
                         <Link to="/about">
-                            <h1><i className="fas fa-users"></i> About Us</h1>
+                            <h1>
+                                <i className="fas fa-users"></i>
+                                <span> About Us</span>
+                            </h1>
                         </Link>
                         <Link to="/">
-                            <h1><i className="fas fa-home"></i> Home</h1>
+                            <h1>
+                                <i className="fas fa-home"></i>
+                                <span> Home</span>
+                            </h1>
+                        </Link>
+                        <Link to="/contact">
+                            <h1>
+                                <i className="fas fa-address-book"></i>
+                                <span>  Contact</span>
+                            </h1>
                         </Link>
                     </div>
                 </div>
